2b3ec3a038006195d601f73445c97a1cd9484507
pay <ka79411.yt@gmail.com>
2025-10-03 21:01:25 +0800

    Defines the token structure.

--- tokenizer.c
+++ tokenizer.c
@@ -1,3 +1,24 @@
 // tokenizer.c
 // Can tokenize expressions.
 // Defines token type.
+
+// Token types.
+enum TokenType {
+  TYPE_NUMBER,
+  TYPE_OPERATOR,
+  END
+};
+
+// Token data.
+union TokenData {
+  double number;
+  char operator;
+};
+
+// Token structure.
+struct Token {
+  // Type of this token.
+  enum TokenType type;
+  // Data.
+  union TokenData data;
+};
