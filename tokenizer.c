0237cfbc1b93e19d010f1e14c9ee4b8cd2f192ae
pay <ka79411.yt@gmail.com>
2025-10-03 21:06:35 +0800

    Move type defines to tokenizer.h

--- tokenizer.c
+++ tokenizer.c
@@ -2,23 +2,4 @@
 // Can tokenize expressions.
 // Defines token type.
 
-// Token types.
-enum TokenType {
-  TYPE_NUMBER,
-  TYPE_OPERATOR,
-  END
-};
-
-// Token data.
-union TokenData {
-  double number;
-  char operator;
-};
-
-// Token structure.
-struct Token {
-  // Type of this token.
-  enum TokenType type;
-  // Data.
-  union TokenData data;
-};
+#include "tokenizer.h"
---
+++ tokenizer.h
@@ -0,0 +1,28 @@
+// tokenizer.h
+// Defines types about token.
+
+#ifndef _ALGORITHM_CALCULATOR_TOKENIZER_H_
+#define _ALGORITHM_CALCULATOR_TOKENIZER_H_
+
+// Token types.
+enum TokenType {
+  TYPE_NUMBER,
+  TYPE_OPERATOR,
+  END
+};
+
+// Token data.
+union TokenData {
+  double number;
+  char operator;
+};
+
+// Token structure.
+struct Token {
+  // Type of this token.
+  enum TokenType type;
+  // Data.
+  union TokenData data;
+};
+
+#endif  // _ALGORITHM_CALCULATOR_TOKENIZER_H_
