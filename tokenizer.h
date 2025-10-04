28be56c1ab670958f68006df7e013a03bf21aa55
pay <ka79411.yt@gmail.com>
2025-10-04 19:27:19 +0800

    Declare functions for Token in header file

--- tokenizer.h
+++ tokenizer.h
@@ -25,4 +25,27 @@ struct Token {
   union TokenData data;
 };
 
+// @brief Init a Token type.
+//
+// @param token Token to init.
+void TokenInit(struct Token *token);
+
+// @brief Set the numeric data to the token's data.
+//
+// @param token The token whose data will be set.
+// @param number The numeric data to assign to the token.
+void TokenSetDataNumber(struct Token *token, double number);
+
+// @brief Assign an operator character to the token's data.
+//
+// @param token The token whose data will be set.
+// @param op The operator character to assign (e.g. '+','-','*','/').
+void TokenSetDataOperator(struct Token *token, char op);
+
+// @brief Get the data type stored in the token.
+//
+// @param token Pointer to the token whose data type is to be retrieved.
+// @return The data type of the token.
+enum TokenType TokenGetDataType(struct Token *token);
+
 #endif  // _ALGORITHM_CALCULATOR_TOKENIZER_H_
