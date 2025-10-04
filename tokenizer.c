9757968052425d9dd4639a89845dd8314912cfa9
pay <ka79411.yt@gmail.com>
2025-10-04 19:31:44 +0800

    Implement functions declared in tokenizer.h

--- tokenizer.c
+++ tokenizer.c
@@ -3,3 +3,21 @@
 // Defines token type.
 
 #include "tokenizer.h"
+
+void TokenInit(struct Token *token) {
+  token->type = UNSET;
+}
+
+void TokenSetDataNumber(struct Token *token, double number) {
+  token->type = TYPE_NUMBER;
+  token->data.number = number;
+}
+
+void TokenSetDataOperator(struct Token *token, char op) {
+  token->type = TYPE_OPERATOR;
+  token->data.op = op;
+}
+
+enum TokenType TokenGetDataType(struct Token *token) {
+  return token->type;
+}
